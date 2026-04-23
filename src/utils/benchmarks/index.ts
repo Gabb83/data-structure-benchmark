db15efc9bf4554bdd860dc0664686fe4eca5f10e
Gabriel Evangelista <evangelistagabriel798@gmail.com>
2026-04-23 20:46:02 -0300

    feat: implementada a estrutura dados set

--- src/utils/benchmarks/index.ts
+++ src/utils/benchmarks/index.ts
@@ -2,11 +2,13 @@ import { Registro } from "../generateData";
 import { benchmarkArray } from "./array";
 import { benchmarkArvoreBinaria } from "./arvoreBinaria";
 import { benchmarkMap } from "./map";
+import { benchmarkSet } from "./set";
 
 export function executar(estrutura: string, operacao: string, dados: Registro[], termo: string): Registro[] {
   if(estrutura === "Array") return benchmarkArray(operacao, dados, termo);
   if(estrutura === "Map") return benchmarkMap(operacao, dados, termo);
   if(estrutura === "Árvore Binária") return benchmarkArvoreBinaria(operacao, dados, termo);
+  if(estrutura === "Set") return benchmarkSet(operacao, dados, termo);
   
   return [];
 }
